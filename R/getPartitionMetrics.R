059ade33d0a12a9628f3c24a9997395f374ff8ee
Siyuan Luo <roseluosy@gmail.com>
2024-05-31 13:41:48 +0200

    add getPartitionMetrics.R

---
+++ R/getPartitionMetrics.R
@@ -0,0 +1,59 @@
+#' getPartitionMetrics
+#' 
+#' Short description...
+#'
+#' @param true A vector containing the labels of the true classes. Must be a 
+#'  vector of characters, integers, numerics, or a factor, but not a list.
+#' @param pred A vector containing the labels of the predicted clusters. Must 
+#'  be a vector of characters, integers, numerics, or a factor, but not a list.
+#' @param metrics The metrics to compute. If omitted, main metrics will be 
+#'   computed.
+#' @return A list of metrics.
+#' @details
+#' Additional details...
+#' 
+#' 
+#' @export
+#' @examples
+#' true <- rep(LETTERS[1:3], each=10)
+#' pred <- c(rep("A", 8), rep("B", 9), rep("C", 3), rep("D", 10))
+#' getPartitionMetrics(true, pred)
+setGeneric("getPartitionMetrics", 
+           function(true, pred, metrics, ...) standardGeneric("getPartitionMetrics"))
+
+setMethod("getPartitionMetrics", signature="ANY",
+          definition=function(true, pred, ...){
+            if (anyNA(true) | anyNA(pred))
+              stop("NA are not supported.")
+            if (((!is.vector(true) & !is.factor(true)) | is.list(true)) | ((!is.vector(pred) & !is.factor(pred)) | is.list(pred)))
+              stop("true and pred must be vectors or factors but not lists.")
+            if(length(true) != length(pred)){
+              stop("The two input vectors should have the same length.")
+            }
+            .getPartitionMetrics(true, pred, ...)
+          })
+
+#' @importFrom aricode sortPairs
+.getPartitionMetrics <-function(true, pred, metrics=c("ARI", "AW", "AV"), ...){
+  res <- sortPairs(true, pred)
+  n <- length(true)
+
+  spairs <- n*(n-1)/2 # N
+  stot <- sum(choose(res$nij, 2), na.rm=TRUE) # T
+  srow <- sum(choose(res$ni., 2), na.rm=TRUE) # P
+  scol <- sum(choose(res$n.j, 2), na.rm=TRUE) # Q
+  a <- stot
+  b <- srow-stot
+  c <- scol-stot
+  d <- spairs+stot-srow-scol
+  
+  res <- lapply(setNames(metrics,metrics), FUN=function(m){
+    switch(m,
+           AW = (a*d-b*c)/((a+b)*(b+d)),
+           AV = (a*d-b*c)/((a+c)*(c+d)),
+           ARI = 2*(a*d-b*c)/((a+b)*(b+d)+(a+c)*(c+d)),
+           stop("Unknown metric.")
+    )
+  })
+  return(res)
+}
