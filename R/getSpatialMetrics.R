684914e4bf6e6075e42f00c831e8ba70786fb1cb
Siyuan Luo <roseluosy@gmail.com>
2024-09-30 21:41:46 +0200

    add docs and examples for spatial function

--- R/getSpatialMetrics.R
+++ R/getSpatialMetrics.R
@@ -75,10 +75,21 @@ getSpatialInternalMetrics <- function(label, location, k=6,
   return(res)
 }
 
-#' getSpatiaGloballExternalMetrics
+#' Compute global-level external evaluation metrics for spatially-resolved data
 #' 
 #' Computes a selection of external clustering evaluation metrics for spatial 
-#' data at the global level.
+#' data at the global level. Options include a series of fuzzy pair-counting 
+#' metrics and set matching-based accuracy.
+#' @inheritParams getFuzzyPartitionMetrics
+#' @param k The number of neighbors used when calculating the fuzzy 
+#' class memberships for fuzzy metrics, or when calculating the weighted
+#' accuracy.
+#' @param metrics a vector of metric names to compute. 
+#' @param ... Optional params for [getFuzzyPartitionMetrics()] or [nnWeightedAccuracy].
+#' @examples
+#' data <- sp_toys
+#' getSpatialGlobalExternalMetrics(data$label, data$p1, data[,c("x", "y")], k=6)
+#' getSpatialGlobalExternalMetrics(data$label, data$p2, data[,c("x", "y")], k=6)
 getSpatialGlobalExternalMetrics <- function(true, pred, location, k=6, 
                                             metrics=c("SpatialRI","SpatialARI",
                                                       "SpatialWH","SpatialAWH", 
@@ -117,10 +128,18 @@ getSpatialGlobalExternalMetrics <- function(true, pred, location, k=6,
   return(unlist(res))
 }
 
-#' getSpatialClassExternalMetrics
+#' Compute class-level external evaluation metrics for spatially-resolved data
 #' 
 #' Computes a selection of external clustering evaluation metrics for spatial 
-#' data at the class/cluster level.
+#' data at the class/cluster level. 
+#' @inheritParams getSpatialGlobalExternalMetrics
+#' @param k The number of neighbors used when calculating the fuzzy 
+#' class memberships for fuzzy metrics.
+#' @param ... Optional params for [getFuzzyPartitionMetrics()].
+#' @examples
+#' data <- sp_toys
+#' getSpatialClassExternalMetrics(data$label, data$p1, data[,c("x", "y")], k=6)
+#' getSpatialClassExternalMetrics(data$label, data$p2, data[,c("x", "y")], k=6)
 getSpatialClassExternalMetrics <- function(true, pred, location, k=6, 
                                            metrics=c("SpatialWH","SpatialAWH", 
                                                      "SpatialWC","SpatialAWC"), 
