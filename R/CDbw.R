1d62b7acb04db08925a5692c14f84c0e9e742c29
plger <pl.germain@gmail.com>
2024-10-07 12:52:02 +0200

    convert factor labels to integer

--- R/CDbw.R
+++ R/CDbw.R
@@ -39,6 +39,7 @@
 #' CDbw(d1[,1:2], d1[,3])
 CDbw <- function(x, labels, r=10, s=seq(0.1,0.8,by=0.1),
                  clusterstdev=TRUE, trace=FALSE, ...){
+  labels <- as.integer(as.factor(labels))
   p <- ncol(x)
   n <- nrow(x)
   x <- as.matrix(x)
