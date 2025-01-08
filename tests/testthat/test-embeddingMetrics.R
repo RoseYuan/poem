d15d2d55248d5c5f38467f418847e98f1fda97fd
plger <pl.germain@gmail.com>
2025-01-08 14:50:38 +0100

    extra tests

--- tests/testthat/test-embeddingMetrics.R
+++ tests/testthat/test-embeddingMetrics.R
@@ -4,6 +4,7 @@ d1 <- mockData()
 d2 <- mockData(classDiff=4)
 
 test_that("Embedding metrics work", {
+  m1 <- getEmbeddingMetrics(d1[,1:2], d1$class, level="class", shared=TRUE)
   m1 <- getEmbeddingMetrics(d1[,1:2], d1$class, level="class")
   m2 <- getEmbeddingMetrics(d1[,1:2], d1$class, level="dataset", 
                             metrics=c("meanSW", "meanClassSW", "pnSW", "minClassSW", "cdbw", "cohesion", "compactness", "sep", "dbcv"))
@@ -14,4 +15,9 @@ test_that("Embedding metrics work", {
   expect_true(all(m1$pnSW>=0 & m1$pnSW<=1))
   expect_true(m2$meanSW<m3$meanSW & m2$meanClassSW<m3$meanClassSW & 
                 m2$pnSW>m3$pnSW & m2$cdbw<m3$cdbw)
+})
+
+test_that("Dist input works", {
+  m1 <- getEmbeddingMetrics(dist(d1[,1:2]), d1$class, level="class", shared=TRUE)
+  expect_true(!any(is.na(as.matrix(m1[,-1]))))
 })
