776a41f88a0cc0cfdf00c454079366e52e7b6c68
Siyuan Luo <roseluosy@gmail.com>
2025-01-12 17:09:46 +0100

    add data scripts to data doc

--- R/data.R
+++ R/data.R
@@ -1,6 +1,7 @@
 #' @title The noisy moon dataset
 #' @description
-#' A simple toy dataset consists of two interleaving half circles.
+#' A simple toy dataset consists of two interleaving half circles. The code to generate the 
+#' dataset is at system.file('inst/scripts/', 'noisy_moon.R', package='poem').
 #'
 #' @format ## `noisy_moon`
 #' A data frame with 100 rows and 5 columns:
@@ -15,7 +16,8 @@
 
 #' @title Toy examples of spatial data
 #' @description
-#' Toy examples of spatial data. 
+#' Toy examples of spatial data. The code to generate the dataset is at 
+#' system.file('inst/scripts/', 'sp_toys.R', package='poem').
 #' 
 #' @format ## `sp_toys`
 #' A data frame with 240 rows and 11 columns, representing a 16 x 15 array of spots:
@@ -32,7 +34,8 @@
 #' Toy example 2D embeddings of elements of different classes, with varying mixing and 
 #' spread. Graphs 1-3 all have 20 elements of each of 4 classes, but that are mixed in
 #' different fashion in the embedding space. Graphs 4-7 all have 100 elements of class1
-#' and 60 of class2, and the class1 elements vary in their spread.
+#' and 60 of class2, and the class1 elements vary in their spread. The code to generate 
+#' the dataset is at system.file('inst/scripts/', 'graph_example.R', package='poem').
 #' 
 #' @format ## `toyExamples`
 #' A data frame.
@@ -45,7 +48,9 @@
 
 
 #' @title Metrics Information
-#' @description A dataframe storing the information of all metrics
+#' @description A dataframe storing the information of all metrics. The code to generate 
+#' the dataset is at system.file('inst/scripts/', 'metric_info.R', package='poem').
+
 
 #' @format ## `metric_info`
 #' A data frame.
