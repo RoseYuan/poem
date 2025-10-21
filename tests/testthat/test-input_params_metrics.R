ac7aa73c318aeb50faa8eed574eecea5ff962f9f
plger <pl.germain@gmail.com>
2025-10-21 16:31:29 +0200

    fixed wrong var name in test

--- tests/testthat/test-input_params_metrics.R
+++ tests/testthat/test-input_params_metrics.R
@@ -57,7 +57,7 @@ set.seed(123)
 d3 <- mockData(classDiff=0.5)
 
 test_that("Graph class metrics from data.frame works", {
-  m1 <- getGraphMetrics(d1[,seq_len(2)], d1$class, k=5, level="class")
+  m1 <- getGraphMetrics(em[,seq_len(2)], em$class, k=5, level="class")
   expect_false(any(is.na(as.matrix(m1)) | is.infinite(as.matrix(m1))))
   expect_true(all(m1$NP>=0 & m1$NP<=1))
   expect_true(all(m1$PWC>=0 & m1$PWC<=1))
@@ -65,24 +65,24 @@ test_that("Graph class metrics from data.frame works", {
 })
 
 test_that("Graph class metrics from knn works", {
-  k1 <- emb2knn(as.matrix(d1[,seq_len(2)]), k=5)
-  m1b <- getGraphMetrics(k1, d1$class, level="class", k=5)
+  k1 <- emb2knn(as.matrix(em[,seq_len(2)]), k=5)
+  m1b <- getGraphMetrics(k1, em$class, level="class", k=5)
   expect_false(any(is.na(as.matrix(m1b)) | is.infinite(as.matrix(m1b))))
-  m1e <- getGraphMetrics(k1, d1$class, level="element", k=5,
+  m1e <- getGraphMetrics(k1, em$class, level="element", k=5,
                          metrics=c("SI", "NP"))
 })
 
 test_that("Graph class metrics from graph works", {
-  k1 <- emb2knn(as.matrix(d1[,seq_len(2)]), k=5)
+  k1 <- emb2knn(as.matrix(em[,seq_len(2)]), k=5)
   g <- bluster::neighborsToKNNGraph(k1$index)
-  m1c <- getGraphMetrics(g, d1$class, level="class", k=5)
+  m1c <- getGraphMetrics(g, em$class, level="class", k=5)
   expect_false(any(is.na(as.matrix(m1c)) | is.infinite(as.matrix(m1c))))
-  m1e <- getGraphMetrics(g, d1$class, level="element", k=5,
+  m1e <- getGraphMetrics(g, em$class, level="element", k=5,
                          metrics=c("SI", "NP"))
 })
 
 test_that("Relative graph class metrics are consistent with expectations", {
-  m1 <- getGraphMetrics(d1[,seq_len(2)], d1$class, k=5, level="class")
+  m1 <- getGraphMetrics(em[,seq_len(2)], em$class, k=5, level="class")
   m2 <- getGraphMetrics(d2[,seq_len(2)], d2$class, k=5, level="class")
   m3 <- getGraphMetrics(d3[,seq_len(2)], d3$class, k=5, level="class")
   expect_gte(m2$AMSP[1], m1$AMSP[1])
@@ -93,15 +93,15 @@ test_that("Relative graph class metrics are consistent with expectations", {
 })
 
 test_that("Element-level graph metrics work", {
-  m <- getGraphMetrics(d1[,seq_len(2)], d1$class, k=5, level="element",
+  m <- getGraphMetrics(em[,seq_len(2)], em$class, k=5, level="element",
                        metrics=c("SI","NP","NCE"))
-  expect_equal(nrow(m),nrow(d1))
+  expect_equal(nrow(m),nrow(em))
   expect_true(all(m$SI>=0 & m$SI<=1))
   expect_true(all(m$NP>=0 & m$NP<=1))
 })
 
 test_that("Dataset-level graph metrics work", {
-  m <- getGraphMetrics(d1[,seq_len(2)], d1$class, k=5, level="dataset",
+  m <- getGraphMetrics(em[,seq_len(2)], em$class, k=5, level="dataset",
                        metrics=c("SI","AMSP","PWC","cohesion"))
   expect_equal(nrow(m),1) 
   expect_equal(m$cohesion,0)
